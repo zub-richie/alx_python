e8e34b8cdac80c530da3e9d263c7f60287d71a4e
zub-richie <zub.richie@gmail.com>
2023-07-26 12:14:59 +0100

    Tast 4 - Print hexadecimal

--- python-hello_world/4-print_hexa.py
+++ python-hello_world/4-print_hexa.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 # for i in range(0, 99):
 #     print(i, hex(i))
-for i in range(0, 99):
-    print("{:02} {}".format(i, hex(i)))
+# for i in range(0, 99):
+#     print("{:02} {}".format(i, hex(i)))
+for i in range(99):
+    print("%02d %s" % (i, hex(i)))
