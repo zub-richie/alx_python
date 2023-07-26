bc2c4fd9f5708a480928bffb775d278916781d79
zub-richie <zub.richie@gmail.com>
2023-07-26 15:35:27 +0100

    Task 4. Fibonacci Sequence Function

--- python-functions/4-fibonacci.py
+++ python-functions/4-fibonacci.py
@@ -1,9 +1,15 @@
 #!/usr/bin/env python3
 def fibonacci_sequence(n):
+    if n == 0:
+        return []
+    if n == 1:
+        return [0]
     fibonacci_list = []
     fibonacci_list.append(0)
     fibonacci_list.append(1)
-    for i in range (2, n):
+
+    for i in range(2, n):
         next_number = fibonacci_list[-1] + fibonacci_list[-2]
         fibonacci_list.append(next_number)
+
     return fibonacci_list
