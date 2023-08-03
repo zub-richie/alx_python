b17b3bca5b6c49358525937ac1b02f37f4ef2e57
zub-richie <zub.richie@gmail.com>
2023-08-03 01:39:22 +0100

    Task 0. Import a function from a file

--- python-import_modules/add_0.py
+++ python-import_modules/add_0.py
@@ -11,5 +11,10 @@ def add(a, b):
     """
     return (a + b)
 if __name__ == "__main__":
-    result = add(3, 5)
-    print("The result of addition is:", result)
+    from add_0 import add
+
+    a = 1
+    b = 2
+    result = add(a, b)
+
+    print("{:d} + {:d} = {:d}".format(a, b, result))
