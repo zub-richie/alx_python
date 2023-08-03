e975c5ceabeafdb5127188c64db8a946aae90198
zub-richie <zub.richie@gmail.com>
2023-08-03 03:34:56 +0100

    Task 1. How to make a script dynamic!

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -4,8 +4,11 @@ if __name__ == "__main__":
 
     inputArgs = sys.argv
 
-    print (len(inputArgs) - 1, "arguments:")
-
+    # print (len(inputArgs) - 1, "arguments:")
+    if len(inputArgs) > 1:
+        print(len(inputArgs) - 1, "arguments:")
+    else:
+        print (len(inputArgs) - 1, "arguments.")
     for i, inputArgs in enumerate(inputArgs[1:], 1):
         # print(f"{i}: {arg}")
         print("{}: " "{}".format(i, inputArgs))
