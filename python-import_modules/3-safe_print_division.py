0160bc6223de972f1f7120ca1752158375656aee
zub-richie <zub.richie@gmail.com>
2023-08-08 19:46:46 +0100

    Task 3. Integers division with debug

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -6,14 +6,3 @@ def safe_print_division(a, b):
     finally:
         print("Inside result: {}".format(result))
     return result
-
-# Using the function
-a = 12
-b = 2
-result = safe_print_division(a, b)
-print("{:d} / {:d} = {}".format(a, b, result))
-
-a = 12
-b = 0
-result = safe_print_division(a, b)
-print("{:d} / {:d} = {}".format(a, b, result))
