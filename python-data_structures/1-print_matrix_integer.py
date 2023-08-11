e2d8ab6cb63344126813edccfe710e770f607e83
zub-richie <zub.richie@gmail.com>
2023-08-11 06:20:35 +0100

    Task 1. Lists of lists = Matrix

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -5,11 +5,3 @@ def print_matrix_integer(matrix=[[]]):
             print("{:d}".format(num), end=" ")  # Step 3
         print()  # Print a newline character after each row
 
-# Test the function
-
-matrix = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-]
-
