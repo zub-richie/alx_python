3974c56c65a97d12373fd72785cc4ace48a5da2b
zub-richie <zub.richie@gmail.com>
2023-08-11 07:04:22 +0100

    Task 1. Lists of lists = Matrix

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -3,4 +3,4 @@ def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for num in row:
             print("{:d}".format(num), end=" ")
-        print(sep=None)
+        print(sep=None, end='\n')
