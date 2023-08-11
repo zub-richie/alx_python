def6c89e2653d2ca8d58bdea7293ce67cc5c3876
zub-richie <zub.richie@gmail.com>
2023-08-11 06:23:20 +0100

    Task 2. More returns!

--- python-data_structures/2-multiple_returns.py
+++ python-data_structures/2-multiple_returns.py
@@ -7,8 +7,3 @@ def multiple_returns(sentence):
     else:
         first = sentence[0]
     return length, first
-
-sentence = ()
-length, first = multiple_returns(sentence)
-print("Length: {:d} - First character: {}".format(length, first))
-
