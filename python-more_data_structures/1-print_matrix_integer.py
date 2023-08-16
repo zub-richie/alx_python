dc6b1557c82302e14732a333c18d5a37f2922509
zub-richie <zub.richie@gmail.com>
2023-08-16 12:47:54 +0100

    Task 0. Squared simple

---
+++ python-more_data_structures/1-print_matrix_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def square_matrix_simple(matrix=[]):
+    new_matrix = []  #e
+    
+    for row in matrix:  
+        squared_row = list(map(lambda x: x ** 2, row))  
+        new_matrix.append(squared_row)  
+    
+    return new_matrix  
+
