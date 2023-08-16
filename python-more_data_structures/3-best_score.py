b10380c24d6da5caacf3b4e9d316fb8d03549606
zub-richie <zub.richie@gmail.com>
2023-08-16 14:42:53 +0100

    Task 3. Best score

--- python-more_data_structures/3-best_score.py
+++ python-more_data_structures/3-best_score.py
@@ -1,6 +1,19 @@
 #!/usr/bin/python3
 
+# def best_score(a_dictionary):
+#     max_score = None
+#     best_key = None
+    
+#     for key, value in a_dictionary.items():
+#         if max_score is None or value > max_score:
+#             max_score = value
+#             best_key = key
+    
+#     return best_key
 def best_score(a_dictionary):
+    if a_dictionary is None:  # Check if the dictionary is None
+        return None
+
     max_score = None
     best_key = None
     
