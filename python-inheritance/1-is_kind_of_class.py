6a9ea5dcdb0f12bc23177b3500df314b68008ed9
zub-richie <zub.richie@gmail.com>
2023-09-11 18:51:41 +0100

    Task 1. Same class or inherit from

---
+++ python-inheritance/1-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""
+This module defines if a an instance.
+"""
+def is_kind_of_class(obj, a_class):
+    """Check if obj is an instance of, or inherits from, the specified class."""
+    return isinstance(obj, a_class)
