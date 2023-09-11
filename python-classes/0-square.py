24411ab29ca3657b1c6119300a10d392446c18de
zub-richie <zub.richie@gmail.com>
2023-09-11 17:35:47 +0100

    Task 0. Square with size

---
+++ python-classes/0-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+class Square:
+    def __init__(self, size):
+        self.__size = size
+
+    def draw(self):
+        for _ in range(self.__size):
+            print("* " * self.__size)
