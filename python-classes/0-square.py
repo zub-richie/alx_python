50a16babd44eb664cdd54d7d56c30dfd94c35bfe
zub-richie <zub.richie@gmail.com>
2023-09-11 18:24:18 +0100

    Task 0. Square with size

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,6 +1,17 @@
 #!/usr/bin/python3
 
+"""
+This module defines a Square class.
+"""
+
 class Square:
+    """
+    This class represents a square.
+
+    Attributes:
+        __size (int): The size of the square.
+    """
+
     def __init__(self, size):
         """
         Initializes a new Square instance with a given size.
