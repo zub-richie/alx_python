9f7dd91561babab802a4d8323162399f1e8d5a59
zub-richie <zub.richie@gmail.com>
2023-09-11 18:10:45 +0100

    Task 0. Square with size

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,18 +1,33 @@
 #!/usr/bin/python3
 
 class Square:
-    #   This class represents a square.
+    """
+    This class represents a square.
+
+    Attributes:
+        __size (int): The size of the square.
+    """
 
     def __init__(self, size):
-        # Initializes a new Square instance with a given size.
+        """
+        Initializes a new Square instance with a given size.
 
+        Args:
+            size (int): The size of the square.
+        """
         self.__size = size
 
     def draw(self):
-        # Draws the square as an ASCII art representation.
-        
+        """
+        Draws the square as an ASCII art representation.
+        """
         for _ in range(self.__size):
             print("* " * self.__size)
 
-# Create a square with size 3
-        return 0
+# Check if this script is being run as the main module
+if __name__ == "__main__":
+    # Create a square with size 3
+    my_square = Square(3)
+    my_square.draw()
+
+
