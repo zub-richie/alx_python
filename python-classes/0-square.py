2b95767d6ad1513522db95355fd5e8a318ceee0a
zub-richie <zub.richie@gmail.com>
2023-09-11 17:36:51 +0100

    Task 0. Square with size

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -7,3 +7,18 @@ class Square:
     def draw(self):
         for _ in range(self.__size):
             print("* " * self.__size)
+
+# Create a square with size 3
+my_square = Square(3)
+print(type(my_square))
+print(my_square.__dict__)
+
+try:
+    print(my_square.size)
+except Exception as e:
+    print(e)
+
+try:
+    print(my_square.__size)
+except Exception as e:
+    print(e)
