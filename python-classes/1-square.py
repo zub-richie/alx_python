913cc3dda1e76f905f9b0e70b5bbdd5b19f62a6c
zub-richie <zub.richie@gmail.com>
2023-09-11 18:27:10 +0100

    Task 1. Size validation

---
+++ python-classes/1-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+class Square:
+    def __init__(self, size=0):
+        """
+        Initializes a new Square instance with an optional size.
+
+        Args:
+            size (int, optional): The size of the square. Defaults to 0.
+        
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
