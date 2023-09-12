f6bfc8e394e6f46a343d7d48d5187a2b413df7d0
zub-richie <zub.richie@gmail.com>
2023-09-12 12:41:46 +0100

    Task 7. Full rectangle

--- python-inheritance/7-rectangle.py
+++ python-inheritance/7-rectangle.py
@@ -1,17 +1,24 @@
 #!/usr/bin/python3
-
-"""This module defines the Rectangle class."""
-
+"""
+    This module defines the Rectangle class.
+    """
 class Rectangle(BaseGeometry):
-    """A class representing a rectangle."""
+    """
+    This module defines the __init__ class.
+    """
     def __init__(self, width, height):
+        """A class representing a rectangle."""
         self.integer_validator("width", width)
         self.integer_validator("height", height)
         self.__width = width
         self.__height = height
-
+    """
+    This module defines the area class.
+    """
     def area(self):
         return self.__width * self.__height
-
+    """
+    This module defines the __str__ class.
+    """
     def __str__(self):
         return "[Rectangle] {}/{}".format(self.__width, self.__height)
