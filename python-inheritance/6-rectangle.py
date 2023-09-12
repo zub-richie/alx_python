03c7abb5b38a7f7abd2b73bba93e2f93dd8d50d5
zub-richie <zub.richie@gmail.com>
2023-09-12 12:34:19 +0100

    Task 6. Rectangle

--- python-inheritance/6-rectangle.py
+++ python-inheritance/6-rectangle.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python3
-"""
-This module defines if a an instance.
-"""
+
+"""This module defines the Rectangle class."""
+
 class Rectangle(BaseGeometry):
-    """This is a class"""
+    """A class representing a rectangle."""
+
     def __init__(self, width, height):
-        """This is a class"""
         self.integer_validator("width", width)
         self.integer_validator("height", height)
         self.__width = width
         self.__height = height
-    """This is a class"""
+
     def __str__(self):
-        """This is a class"""
         return "[Rectangle] {}/{}".format(self.__width, self.__height)
