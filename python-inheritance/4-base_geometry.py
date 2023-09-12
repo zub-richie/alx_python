8ce40a7cd641cd5411d8437cc18675cfe4451fe6
zub-richie <zub.richie@gmail.com>
2023-09-12 05:47:22 +0100

    Task 4. Improve Geometry

---
+++ python-inheritance/4-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""
+This module defines if a an instance .
+"""
+class BaseGeometry:
+    """This is a class"""
+    def area(self):
+        raise Exception("area() is not implemented")
