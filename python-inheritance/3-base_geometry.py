abb4e2176ceea377d20ed7dee2c289da7ca8a30d
zub-richie <zub.richie@gmail.com>
2023-09-12 05:44:03 +0100

    Task 3. Geometry module

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -4,5 +4,4 @@ This module defines if a an instance .
 """
 class BaseGeometry:
     """This is a class"""
-    def some_method(self):
-        raise NotImplementedError("Subclasses must implement this method.")
+    pass
